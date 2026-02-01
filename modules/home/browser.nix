14225ffa8a0602e4eb9df8e70545c4167876cd68
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-02-01 16:43:02 +0100

    feat: add zen mods

--- modules/home/browser.nix
+++ modules/home/browser.nix
@@ -25,24 +25,26 @@
         DontCheckDefaultBrowser = true;
         NoDefaultBookmarks = true;
         OfferToSaveLogins = false;
+
         EnableTrackingProtection = {
           Value = true;
           Locked = true;
           Cryptomining = true;
           Fingerprinting = true;
         };
+
         SanitizeOnShutdown = {
           FormData = true;
           Cache = true;
         };
 
         ExtensionSettings = {
-          # uBlock Origin
+          # uBlock Origin - ad blocker
           "uBlock0@raymondhill.net" = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
             installation_mode = "force_installed";
           };
-          # SponsorBlock
+          # SponsorBlock - skip YouTube sponsors
           "sponsorBlocker@ajay.app" = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi";
             installation_mode = "force_installed";
@@ -52,7 +54,7 @@
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/youtube-shorts-block/latest.xpi";
             installation_mode = "force_installed";
           };
-          # Wappalyzer
+          # Wappalyzer - technology profiler
           "wappalyzer@crunchlabz.com" = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/wappalyzer/latest.xpi";
             installation_mode = "force_installed";
@@ -62,7 +64,7 @@
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/react-devtools/latest.xpi";
             installation_mode = "force_installed";
           };
-          # Proton Pass
+          # Proton Pass - password manager (PINNED to navbar)
           "78272b6fa58f4a1abaac99321d503a20@proton.me" = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/proton-pass/latest.xpi";
             installation_mode = "force_installed";
@@ -156,6 +158,7 @@
         };
 
         Preferences = {
+          # UI/UX Preferences
           "browser.aboutConfig.showWarning" = {
             Value = false;
             Status = "locked";
@@ -188,42 +191,8 @@
             Value = false;
             Status = "locked";
           };
-          "privacy.resistFingerprinting" = {
-            Value = true;
-            Status = "locked";
-          };
-          "privacy.resistFingerprinting.randomization.canvas.use_siphash" = {
-            Value = true;
-            Status = "locked";
-          };
-          "privacy.resistFingerprinting.randomization.daily_reset.enabled" = {
-            Value = true;
-            Status = "locked";
-          };
-          "privacy.resistFingerprinting.randomization.daily_reset.private.enabled" = {
-            Value = true;
-            Status = "locked";
-          };
-          "privacy.resistFingerprinting.block_mozAddonManager" = {
-            Value = true;
-            Status = "locked";
-          };
-          "privacy.spoof_english" = {
-            Value = 1;
-            Status = "locked";
-          };
-          "privacy.firstparty.isolate" = {
-            Value = true;
-            Status = "locked";
-          };
-          "network.cookie.cookieBehavior" = {
-            Value = 5;
-            Status = "locked";
-          };
-          "dom.battery.enabled" = {
-            Value = false;
-            Status = "locked";
-          };
+
+          # Performance
           "gfx.webrender.all" = {
             Value = true;
             Status = "locked";
@@ -236,28 +205,197 @@
             Value = true;
             Status = "locked";
           };
+
+          # Security - Force HTTPS only mode
           "dom.security.https_only_mode" = {
             Value = true;
             Status = "locked";
           };
+
+          # Content blocking - strict tracking protection
           "browser.contentblocking.category" = {
             Value = "strict";
             Status = "locked";
           };
+
+          # Enable middle-click autoscroll
           "general.autoScroll" = {
             Value = true;
             Status = "locked";
           };
+
+          # Enable find-as-you-type (press / to search)
           "accessibility.typeaheadfind" = {
             Value = true;
             Status = "locked";
           };
+
+          # Firefox Behavior
+          "browser.shell.checkDefaultBrowser" = {
+            Value = false;
+            Status = "locked";
+          };
+          "browser.startup.homepage" = {
+            Value = "chrome://browser/content/blanktab.html";
+            Status = "locked";
+          };
+          "browser.newtabpage.enabled" = {
+            Value = false;
+            Status = "locked";
+          };
+          "browser.tabs.loadBookmarksInTabs" = {
+            Value = true;
+            Status = "locked";
+          };
+          "browser.ctrlTab.sortByRecentlyUsed" = {
+            Value = true;
+            Status = "locked";
+          };
+          "browser.urlbar.shortcuts.bookmarks" = {
+            Value = false;
+            Status = "locked";
+          };
+          "browser.urlbar.shortcuts.tabs" = {
+            Value = false;
+            Status = "locked";
+          };
+          "browser.bookmarks.showMobileBookmarks" = {
+            Value = false;
+            Status = "locked";
+          };
+          "browser.formfill.enable" = {
+            Value = false;
+            Status = "locked";
+          };
+          "browser.search.suggest.enabled" = {
+            Value = false;
+            Status = "locked";
+          };
+          "browser.download.useDownloadDir" = {
+            Value = true;
+            Status = "locked";
+          };
+          "browser.download.always_ask_before_handling_new_types" = {
+            Value = false;
+            Status = "locked";
+          };
+          "browser.search.separatePrivateDefault" = {
+            Value = false;
+            Status = "locked";
+          };
+
+          # Privacy
+          "privacy.donottrackheader.enabled" = {
+            Value = true;
+            Status = "locked";
+          };
+          "privacy.clearOnShutdown_v2.formdata" = {
+            Value = true;
+            Status = "locked";
+          };
+          "signon.rememberSignons" = {
+            Value = false;
+            Status = "locked";
+          };
+          "dom.security.https_only_mode_ever_enabled" = {
+            Value = true;
+            Status = "locked";
+          };
+          "network.dns.disablePrefetch" = {
+            Value = true;
+            Status = "locked";
+          };
+          "network.prefetch-next" = {
+            Value = false;
+            Status = "locked";
+          };
+          "network.predictor.enabled" = {
+            Value = false;
+            Status = "locked";
+          };
+          "network.http.speculative-parallel-limit" = {
+            Value = 0;
+            Status = "locked";
+          };
+
+          # Devtools
+          "devtools.cache.disabled" = {
+            Value = true;
+            Status = "locked";
+          };
+
+          # Extensions
+          "extensions.autoDisableScopes" = {
+            Value = 0;
+            Status = "locked";
+          };
+
+          # Locale settings
+          "intl.accept_languages" = {
+            Value = "en,no";
+            Status = "locked";
+          };
+          "intl.locale.requested" = {
+            Value = "en-GB";
+            Status = "locked";
+          };
+        };
+
+        # Extension-specific settings
+        "3rdparty" = {
+          Extensions = {
+            "uBlock0@raymondhill.net" = {
+              settings = {
+                selectedFilterLists = [
+                  "ublock-filters"
+                  "ublock-badware"
+                  "ublock-privacy"
+                  "ublock-unbreak"
+                  "ublock-quick-fixes"
+                  "fanboy-cookiemonster"
+                  "easylist-cookie"
+                  "adguard-cookies"
+                  "adguard-popup"
+                  "adguard-mobile"
+                  "adguard-spyware"
+                  "block-annoyances"
+                  "adguard-social"
+                ];
+              };
+            };
+          };
         };
       };
+
       profiles.default = {
         isDefault = true;
         settings = {
+          # Zen Browser UI/Workflow Settings
+          "zen.workspaces.continue-where-left-off" = true;
+          "zen.workspaces.natural-scroll" = true;
+          "zen.view.compact.hide-tabbar" = true;
+          "zen.view.compact.hide-toolbar" = true;
+          "zen.view.compact.animate-sidebar" = false;
+          "zen.welcome-screen.seen" = true;
+          "zen.urlbar.behavior" = "float";
+          "zen.view.experimental-no-window-controls" = true;
+          "zen.view.show-bottom-border" = true;
+          "zen.view.show-newtab-button-top" = false;
+          "zen.tabs.show-newtab-vertical" = false;
+          "zen.theme.color-prefs.amoled" = true;
+          "zen.theme.color-prefs.use-workspace-colors" = true;
+          "zen.themes.updated-value-observer" = false;
+          "zen.splitView.change-on-hover" = true;
+          "zen.glance.enabled" = false;
+          "zen.glance.activation-method" = "meta";
+          "zen.workspaces.force-container-workspace" = true;
+          "zen.pinned-tab-manager.restore-pinned-tabs-to-pinned-url" = true;
+          "zen.theme.accent-color" = "#89b4fa";
 
+          # Font settings
+          "font.name.serif.x-western" = "Roboto";
+          "font.name.sans-serif.x-western" = "Roboto";
+          "font.name.monospace.x-western" = "Roboto Mono";
         };
         userChrome = ''
           :root {
@@ -268,6 +406,26 @@
             display: none;
           }
         '';
+        mods = [
+          "253a3a74-0cc4-47b7-8b82-996a64f030d5" # Floating History
+          "4ab93b88-151c-451b-a1b7-a1e0e28fa7f8" # No Sidebar Scrollbar
+          "7190e4e9-bead-4b40-8f57-95d852ddc941" # Tab title fixes
+          "803c7895-b39b-458e-84f8-a521f4d7a064" # Hide Inactive Workspaces
+          "b51ff956-6aea-47ab-80c7-d6c047c0d510" # Disable Status Bar
+          "a6335949-4465-4b71-926c-4a52d34bc9c0" # Better Find Bar
+          "c6813222-6571-4ba6-8faf-58f3343324f6" # Disable Rounded Corners
+          "c8d9e6e6-e702-4e15-8972-3596e57cf398" # Zen Back Forward
+          "cb15abdb-0514-4e09-8ce5-722cf1f4a20f" # Hide Extension Name
+          "d8b79d4a-6cba-4495-9ff6-d6d30b0e94fe" # Better Active Tab
+          "e122b5d9-d385-4bf8-9971-e137809097d0" # No Top Sites
+          "f7c71d9a-bce2-420f-ae44-a64bd92975ab" # Better Unloaded Tabs
+          "fd24f832-a2e6-4ce9-8b19-7aa888eb7f8e" # Quietify
+          "22c9ec3b-7c62-46ae-991f-c8fff5046829" # Tab Numbers
+          "4c2bec61-7f6c-4e5c-bdc6-c9ad1aba1827" # Vertical Split Tab Groups
+          "4596d8f9-f0b7-4aeb-aa92-851222dc1888" # Only Close On Hover
+          "ae051a40-3e3a-429a-a6f4-199a28b18a75" # Only Reset On Hover
+          "599a1599-e6ab-4749-ab22-de533860de2c" # Pimp your PiP
+        ];
       };
     };
 
