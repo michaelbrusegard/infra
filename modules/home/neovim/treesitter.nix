8e9656598481d9fe4404eae3b5ddee6971e7cd9f
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-02-03 00:39:53 +0100

    feat: add treesitter config to neovim

---
+++ modules/home/neovim/treesitter.nix
@@ -0,0 +1,74 @@
+{ pkgs, ... }: {
+  programs.nvf.settings.vim = {
+    treesitter = {
+      enable = true;
+      highlight.enable = true;
+      indent.enable = true;
+      fold = true;
+      grammars = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+        bash
+        c
+        diff
+        html
+        javascript
+        jsdoc
+        json
+        jsonc
+        lua
+        luadoc
+        luap
+        markdown
+        markdown-inline
+        printf
+        python
+        query
+        regex
+        toml
+        tsx
+        typescript
+        vim
+        vimdoc
+        xml
+        yaml
+      ];
+      textobjects = {
+        enable = true;
+        setupOpts = {
+          move = {
+            enable = true;
+            set_jumps = true;
+            goto_next_start = {
+              "]f" = "@function.outer";
+              "]c" = "@class.outer";
+              "]a" = "@parameter.inner";
+            };
+            goto_next_end = {
+              "]F" = "@function.outer";
+              "]C" = "@class.outer";
+              "]A" = "@parameter.inner";
+            };
+            goto_previous_start = {
+              "[f" = "@function.outer";
+              "[c" = "@class.outer";
+              "[a" = "@parameter.inner";
+            };
+            goto_previous_end = {
+              "[F" = "@function.outer";
+              "[C" = "@class.outer";
+              "[A" = "@parameter.inner";
+            };
+          };
+        };
+      };
+    };
+    treesitter.autotagHtml = true;
+
+    treesitter.context = {
+      enable = true;
+      setupOpts = {
+        mode = "cursor";
+        max_lines = 3;
+      };
+    };
+  };
+}
