5834bb9ef30f144cc2c6cbdc69557d1b766369fb
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-02-02 08:30:55 +0100

    fix: linting stuff

--- modules/home/mpv.nix
+++ modules/home/mpv.nix
@@ -1,5 +1,4 @@
 {
-  pkgs,
   lib,
   isWsl,
   ...
@@ -8,58 +7,60 @@
     enable = true;
   };
 
-  xdg.mimeApps.defaultApplications = {
-    "video/mp4" = ["mpv.desktop"];
-  };
+  xdg = lib.mkIf (!isWsl) {
+    mimeApps.defaultApplications = {
+      "video/mp4" = ["mpv.desktop"];
+    };
 
-  xdg.configFile."mpv/mpv.conf" = lib.mkIf (!isWsl) {
-    text = ''
-      profile = "gpu-hq";
-      vo =
-        if pkgs.stdenv.isDarwin
-        then "libmpv"
-        else "gpu-next";
-      hwdec = "auto-safe";
+    configFile."mpv/mpv.conf" = {
+      text = ''
+        profile = "gpu-hq";
+        vo =
+          if pkgs.stdenv.isDarwin
+          then "libmpv"
+          else "gpu-next";
+        hwdec = "auto-safe";
 
-      scale = "ewa_lanczossharp";
-      cscale = "ewa_lanczossharp";
-      tscale = "oversample";
+        scale = "ewa_lanczossharp";
+        cscale = "ewa_lanczossharp";
+        tscale = "oversample";
 
-      video-sync = "display-resample";
-      interpolation = true;
+        video-sync = "display-resample";
+        interpolation = true;
 
-      audio-file-auto = "fuzzy";
-      audio-channels = "stereo";
+        audio-file-auto = "fuzzy";
+        audio-channels = "stereo";
 
-      volume = "80";
-      volume-max = "200";
+        volume = "80";
+        volume-max = "200";
 
-      sub-auto = "fuzzy";
-      sub-file-paths = "sub:subtitles:Subtitles";
+        sub-auto = "fuzzy";
+        sub-file-paths = "sub:subtitles:Subtitles";
 
-      osd-level = "1";
-      osd-duration = "2000";
-      osd-font-size = "32";
+        osd-level = "1";
+        osd-duration = "2000";
+        osd-font-size = "32";
 
-      screenshot-format = "png";
-      screenshot-png-compression = "8";
-      screenshot-directory = "$HOME/Pictures/screenshots";
+        screenshot-format = "png";
+        screenshot-png-compression = "8";
+        screenshot-directory = "$HOME/Pictures/screenshots";
 
-      cache = "yes";
-      cache-secs = "60";
+        cache = "yes";
+        cache-secs = "60";
 
-      ytdl-format = "bestvideo[height<=?1080]+bestaudio/best"
-    '';
-  };
+        ytdl-format = "bestvideo[height<=?1080]+bestaudio/best"
+      '';
+    };
 
-  xdg.configFile."mpv/input.conf" = lib.mkIf (!isWsl) {
-    text = ''
-      WHEEL_UP add volume 5
-      WHEEL_DOWN add volume -5
-      l seek 5
-      h seek -5
-      j seek -60
-      k seek 60
-    '';
+    configFile."mpv/input.conf" = {
+      text = ''
+        WHEEL_UP add volume 5
+        WHEEL_DOWN add volume -5
+        l seek 5
+        h seek -5
+        j seek -60
+        k seek 60
+      '';
+    };
   };
 }
