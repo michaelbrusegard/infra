3bbca1327345330e7d75a9b0927c104dab458536
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-02-02 09:22:04 +0100

    chore: add config

--- modules/home/zen-browser/default.nix
+++ modules/home/zen-browser/default.nix
@@ -18,7 +18,7 @@
     ./shortcuts.nix
     ./mimeapps.nix
   ];
-  programs.zen-browser = lib.mkIf (!isWsl) {
+  config.programs.zen-browser = lib.mkIf (!isWsl) {
     enable = true;
     darwinDefaultsId = "app.zen-browser.zen";
     languagePacks = ["en-GB"];
