90ec9d0aa2640c97e5208d6e534408a46b77670d
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-02-03 10:10:57 +0100

    fix: minor improvements

--- modules/home/neovim/options.nix
+++ modules/home/neovim/options.nix
@@ -98,6 +98,12 @@
     };
     extraConfigLua = ''
       vim.opt.clipboard = vim.env.SSH_CONNECTION and "" or "unnamedplus"
+
+      vim.filetype.add({
+        extension = {
+          gs = 'javascript',
+        },
+      })
     '';
 
     spellcheck = {
