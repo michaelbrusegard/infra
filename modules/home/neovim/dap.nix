f9b5f9e6711c15313636e0eb4f2a7a7f21266d03
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-02-03 19:08:56 +0100

    feat: add debugger plugin

---
+++ modules/home/neovim/dap.nix
@@ -0,0 +1,95 @@
+{pkgs, ...}: {
+  programs.nvf.settings.vim = {
+    debugger.nvim-dap = {
+      enable = true;
+      ui.enable = true;
+      ui.autoStart = true;
+      mappings = {
+        continue = "<leader>dc";
+        toggleBreakpoint = "<leader>db";
+        stepInto = "<leader>di";
+        stepOver = "<leader>dO";
+        stepOut = "<leader>do";
+        terminate = "<leader>dt";
+        hover = "<leader>dw";
+        runLast = "<leader>dl";
+        runToCursor = "<leader>dC";
+        goDown = "<leader>dj";
+        goUp = "<leader>dk";
+        toggleRepl = "<leader>dr";
+        toggleDapUI = "<leader>du";
+      };
+    };
+
+    lazy.plugins = {
+      "nvim-dap-virtual-text" = {
+        package = pkgs.vimPlugins.nvim-dap-virtual-text;
+      };
+    };
+
+    keymaps = [
+      {
+        mode = "n";
+        key = "<leader>dB";
+        action = "function() require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: ')) end";
+        lua = true;
+        options = {desc = "Breakpoint Condition";};
+      }
+      {
+        mode = "n";
+        key = "<leader>da";
+        action = ''
+          function()
+            local args = vim.fn.input("Run with args: ")
+            require('dap').continue({ before = function(config)
+              config.args = require('dap.utils').splitstr(args)
+              return config
+            end })
+          end
+        '';
+        lua = true;
+        options = {desc = "Run with Args";};
+      }
+      {
+        mode = "n";
+        key = "<leader>dg";
+        action = "function() require('dap').goto_() end";
+        lua = true;
+        options = {desc = "Go to Line (No Execute)";};
+      }
+      {
+        mode = "n";
+        key = "<leader>ds";
+        action = "function() require('dap').session() end";
+        lua = true;
+        options = {desc = "Session";};
+      }
+      {
+        mode = ["n" "x"];
+        key = "<leader>de";
+        action = "function() require('dapui').eval() end";
+        lua = true;
+        options = {desc = "Eval";};
+      }
+    ];
+
+    luaConfigRC.dap-setup = ''
+      -- Icons for DAP
+      local dap_icons = {
+        Stopped             = { "󰁕 ", "DiagnosticWarn", "DapStoppedLine" },
+        Breakpoint          = " ",
+        BreakpointCondition = " ",
+        BreakpointRejected  = { " ", "DiagnosticError" },
+        LogPoint            = ".>",
+      }
+
+      for name, sign in pairs(dap_icons) do
+        sign = type(sign) == "table" and sign or { sign }
+        vim.fn.sign_define(
+          "Dap" .. name,
+          { text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] }
+        )
+      end
+    '';
+  };
+}
