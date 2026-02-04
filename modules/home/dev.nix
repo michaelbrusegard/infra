fdf83017e48e7d8fa137937a32396b128f6bb297
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-02-04 07:47:11 +0100

    chore: add lazysql

--- modules/home/dev.nix
+++ modules/home/dev.nix
@@ -16,6 +16,24 @@
       enable = true;
       enableGitIntegration = true;
     };
+
+    lazysql = {
+      enable = true;
+      settings = {
+        database = [
+          {
+            Name = "Postgres";
+            Provider = "postgres";
+            URL = "postgres://\${env:DB_USER}:\${env:DB_PASSWORD}@localhost:5432/\${env:DB_NAME}";
+          }
+        ];
+        application = {
+          DefaultPageSize = 300;
+          DisableSidebar = false;
+          SidebarOverlay = false;
+        };
+      };
+    };
   };
 
   home = {
