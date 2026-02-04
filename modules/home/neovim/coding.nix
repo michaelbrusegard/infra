67dde3b42a2e0d0dfb75734a1c3a1423ef31592b
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-02-04 08:52:11 +0100

    chore: fix linting

--- modules/home/neovim/coding.nix
+++ modules/home/neovim/coding.nix
@@ -7,38 +7,40 @@
         setup = "require('ts-comments').setup {}";
       };
     };
-    mini.pairs = {
-      enable = true;
-      setupOpts = {
-        modes = {
-          insert = true;
-          command = true;
-          terminal = false;
+    mini = {
+      pairs = {
+        enable = true;
+        setupOpts = {
+          modes = {
+            insert = true;
+            command = true;
+            terminal = false;
+          };
+          # skip autopair when next character is one of these
+          skip_next = ''[%w%%%'%[%"%.%`%$]'';
+          # skip autopair when the cursor is inside these treesitter nodes
+          skip_ts = ["string"];
+          # skip autopair when next character is closing pair
+          # and there are more closing pairs than opening pairs
+          skip_unbalanced = true;
+          # better deal with markdown code blocks
+          markdown = true;
         };
-        # skip autopair when next character is one of these
-        skip_next = ''[%w%%%'%[%"%.%`%$]'';
-        # skip autopair when the cursor is inside these treesitter nodes
-        skip_ts = ["string"];
-        # skip autopair when next character is closing pair
-        # and there are more closing pairs than opening pairs
-        skip_unbalanced = true;
-        # better deal with markdown code blocks
-        markdown = true;
       };
-    };
-    mini.ai.enable = true;
+      ai.enable = true;
 
-    mini.surround = {
-      enable = true;
-      setupOpts = {
-        mappings = {
-          add = "sa";
-          delete = "sd";
-          find = "sf";
-          find_left = "sF";
-          highlight = "sh";
-          replace = "sr";
-          update_n_lines = "sn";
+      surround = {
+        enable = true;
+        setupOpts = {
+          mappings = {
+            add = "sa";
+            delete = "sd";
+            find = "sf";
+            find_left = "sF";
+            highlight = "sh";
+            replace = "sr";
+            update_n_lines = "sn";
+          };
         };
       };
     };
