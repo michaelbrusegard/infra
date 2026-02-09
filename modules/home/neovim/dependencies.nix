e174d33336da1eb33de83f5fa3a24b27190283be
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-02-09 09:54:33 +0100

    chore: add dependencies to neovim

---
+++ modules/home/neovim/dependencies.nix
@@ -0,0 +1,20 @@
+{
+  pkgs,
+  ...
+}: {
+  programs.neovim = {
+    extraPackages = with pkgs; [
+      ripgrep
+      ast-grep
+      fd
+      grpcurl
+      websocat
+      ghostscript
+      imagemagick
+      mermaid-cli
+      jq
+      libxml2
+    ];
+    withNodeJs = true;
+  };
+}
