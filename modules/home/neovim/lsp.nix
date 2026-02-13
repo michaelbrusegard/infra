4f6650215bf9fa52b24f90c4f069a6ad29294e65
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-02-13 21:51:07 +0100

    fix: lsp foldingrange setup

--- modules/home/neovim/lsp.nix
+++ modules/home/neovim/lsp.nix
@@ -6,8 +6,6 @@
   programs.neovim.spec = {
     lsp.onAttach = ''
       vim.keymap.set("n", "<leader>cli", "<cmd>LspInfo<cr>", { buffer = bufnr, desc = "Lsp Info" })
-
-      -- Enhanced Neovim defaults (with which-key descriptions)
       vim.keymap.set("n", "grn", require("live-rename").rename, { buffer = bufnr, desc = "Rename" })
       vim.keymap.set("n", "grr", vim.lsp.buf.references, { buffer = bufnr, desc = "References" })
       vim.keymap.set("n", "gri", vim.lsp.buf.implementation, { buffer = bufnr, desc = "Goto Implementation" })
@@ -16,8 +14,6 @@
       vim.keymap.set("n", "gW", function() require("snacks").picker.lsp_workspace_symbols() end, { buffer = bufnr, desc = "Workspace Symbols" })
       vim.keymap.set({"n", "v"}, "gra", vim.lsp.buf.code_action, { buffer = bufnr, desc = "Code Action" })
       vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "Hover" })
-
-      -- LazyVim additions (no Neovim default)
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr, desc = "Goto Definition" })
       vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = bufnr, desc = "Goto Declaration" })
       vim.keymap.set("n", "gK", vim.lsp.buf.signature_help, { buffer = bufnr, desc = "Signature Help" })
@@ -41,8 +37,8 @@
       end
 
       if client:supports_method("textDocument/foldingRange") then
-        vim.wo[bufnr].foldmethod = "expr"
-        vim.wo[bufnr].foldexpr = "v:lua.vim.lsp.foldexpr()"
+        vim.wo.foldmethod = "expr"
+        vim.wo.foldexpr = "v:lua.vim.lsp.foldexpr()"
       end
     '';
     diagnostics = {
