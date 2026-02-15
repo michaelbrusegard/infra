537b718a768f78eef8f2ede338df5a15e5b1c45d
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-02-15 15:55:39 +0100

    chore: simplify dev default config

--- modules/home/dev.nix
+++ modules/home/dev.nix
@@ -1,9 +1,4 @@
-{
-  pkgs,
-  lib,
-  inputs,
-  ...
-}: {
+_: {
   programs = {
     direnv = {
       enable = true;
@@ -34,32 +29,5 @@
         };
       };
     };
-  };
-
-  home = {
-    packages = with pkgs; [
-      sqlite
-      python3
-      go
-      nodejs
-      lua
-      luarocks
-
-      (inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.stable.withComponents [
-        "rustc"
-        "cargo"
-        "clippy"
-        "rust-src"
-        "rustfmt"
-      ])
-    ];
-
-    sessionVariables =
-      {
-        NODE_COMPILE_CACHE = "$HOME/.cache/nodejs-compile-cache";
-      }
-      // lib.optionalAttrs pkgs.stdenv.isDarwin {
-        LIBRARY_PATH = "${pkgs.libiconv}/lib:$LIBRARY_PATH";
-      };
   };
 }
