26b92f5aa72ca99b10b291b55b4d726c4a2b8c89
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-03-05 23:41:37 +0100

    chore: fix code quality

--- packages/default.nix
+++ packages/default.nix
@@ -1,1 +1,4 @@
-{pkgs}: pkgs.lib.optionalAttrs pkgs.stdenv.isLinux {breaktimer = import ./breaktimer.nix {inherit pkgs;};}
+{pkgs}:
+if builtins.hasAttr "lib" pkgs && builtins.hasAttr "stdenv" pkgs && pkgs.stdenv.isLinux
+then {breaktimer = import ./breaktimer.nix {inherit pkgs;};}
+else {}
