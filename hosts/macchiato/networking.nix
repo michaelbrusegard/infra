dee874102f61a1d9f87255282786528ade4ce0cf
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-03-28 13:04:03 +0100

    fix: entrance-ap mac address

--- hosts/macchiato/networking.nix
+++ hosts/macchiato/networking.nix
@@ -154,7 +154,7 @@ in {
           "tag:br_servers,option:dns-server,10.0.187.1"
         ];
         dhcp-host = [
-          "00:00:00:00:00:01,10.0.186.2,entrance-ap"
+          "90:72:40:04:E7:73,10.0.186.2,entrance-ap"
           "6c:70:9f:ec:10:23,10.0.186.3,basement-ap"
           "6c:70:9f:ec:04:3f,10.0.186.4,office-ap"
           "24:a0:74:73:05:48,10.0.186.5,workshop-ap"
