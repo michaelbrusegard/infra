413e756105f2e5dc570d9dafa8a8d4eac72321e9
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-04-11 03:22:01 +0200

    style: formatting

--- modules/nixos/dsearch.nix
+++ modules/nixos/dsearch.nix
@@ -2,13 +2,7 @@
   # TODO: Remove when updating to nixpkgs 26.05
   imports = [
     (
-      {
-        config,
-        lib,
-        pkgs,
-        options,
-        ...
-      } @ args:
+      args:
         builtins.removeAttrs
         (import "${inputs.nixpkgs-unstable}/nixos/modules/programs/dsearch.nix" args)
         ["meta"]
