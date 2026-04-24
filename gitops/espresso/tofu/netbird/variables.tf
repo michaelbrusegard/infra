4eddd15b7141d0982a7b4dc2098cf5a2ee9a90b0
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-04-24 20:48:14 +0200

    feat: add netbird opentofu config

---
+++ gitops/espresso/tofu/netbird/variables.tf
@@ -0,0 +1,16 @@
+variable "netbird_token" {
+  description = "NetBird management PAT used by tofu-controller"
+  type        = string
+  sensitive   = true
+}
+
+variable "netbird_management_url" {
+  description = "NetBird management API base URL"
+  type        = string
+}
+
+variable "macchiato_blocky_dns_ip" {
+  description = "IP of Blocky on macchiato reachable from NetBird peers"
+  type        = string
+  default     = "10.0.187.1"
+}
