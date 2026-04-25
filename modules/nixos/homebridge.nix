9646846f39d04514b33016f64bd0c82b68852b15
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-04-25 12:47:05 +0200

    feat: add avahi to homebridge

--- modules/nixos/homebridge.nix
+++ modules/nixos/homebridge.nix
@@ -1,5 +1,15 @@
 {config, ...}: {
   services = {
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      publish = {
+        enable = true;
+        userServices = true;
+        addresses = true;
+      };
+    };
+
     homebridge = {
       enable = true;
       settings = {
