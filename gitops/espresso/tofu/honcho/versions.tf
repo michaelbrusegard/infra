a01ba8a2ab87be6596a5f9d2fd90926772b18f3c
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-04-26 20:23:02 +0200

    chore(renovate): track custom dependency pins

--- gitops/espresso/tofu/honcho/versions.tf
+++ gitops/espresso/tofu/honcho/versions.tf
@@ -3,9 +3,9 @@ terraform {
   required_version = "= 1.11.5"
 
   required_providers {
-    netbird = {
-      source  = "netbirdio/netbird"
-      version = "= 0.0.9"
+    postgresql = {
+      source  = "cyrilgdn/postgresql"
+      version = "= 1.26.0"
     }
   }
 }
