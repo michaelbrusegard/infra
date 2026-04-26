1ed26382b14898d88e37b8ca57a7d0c150ab27c4
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-04-26 10:49:10 +0200

    fix: unifi url

--- modules/nixos/unifi.nix
+++ modules/nixos/unifi.nix
@@ -3,4 +3,18 @@ _: {
     "unifi-controller-9.5.21"
   ];
   services.unifi.enable = true;
+
+  # HTTP reverse proxy so the k8s internal gateway can reach Unifi's HTTPS UI
+  services.caddy = {
+    enable = true;
+    virtualHosts."http://0.0.0.0:8444" = {
+      extraConfig = ''
+        reverse_proxy https://127.0.0.1:8443 {
+          transport http {
+            tls_insecure_skip_verify
+          }
+        }
+      '';
+    };
+  };
 }
