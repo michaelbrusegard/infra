653c6c350f08426cccf7477c68ab02f0faf5f594
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-04-26 11:43:19 +0200

    test: try using socat instead of caddy service for macchiato

--- modules/nixos/unifi.nix
+++ modules/nixos/unifi.nix
@@ -1,22 +1,16 @@
-_: {
+{lib, pkgs, ...}: {
   nixpkgs.config.permittedInsecurePackages = [
     "unifi-controller-9.5.21"
   ];
   services.unifi.enable = true;
 
-  # HTTP reverse proxy so the k8s internal gateway can reach Unifi's HTTPS UI
-  services.caddy = {
-    enable = true;
-    virtualHosts."http://0.0.0.0:8444" = {
-      extraConfig = ''
-        reverse_proxy https://127.0.0.1:8443 {
-          header_up Host {host}
-          header_up X-Forwarded-Proto https
-          transport http {
-            tls_insecure_skip_verify
-          }
-        }
-      '';
+  # Plain TCP to TLS bridge so the k8s internal gateway can reach Unifi's HTTPS UI
+  systemd.services.unifi-proxy = {
+    after = ["network.target" "unifi.service"];
+    wantedBy = ["multi-user.target"];
+    serviceConfig = {
+      ExecStart = "${lib.getExe pkgs.socat} TCP-LISTEN:8444,fork,reuseaddr OPENSSL:127.0.0.1:8443,verify=0";
+      Restart = "always";
     };
   };
 }
