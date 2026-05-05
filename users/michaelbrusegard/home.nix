d71db5b8277c388d700a75202f510ac091ad6594
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-05-05 17:51:27 +0200

    fix: reenable darwin sops-nix fix

--- users/michaelbrusegard/home.nix
+++ users/michaelbrusegard/home.nix
@@ -1,4 +1,8 @@
-{inputs, ...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
   imports = [
     inputs.self.homeManagerModules.catppuccin
     inputs.self.homeManagerModules.cli-base
@@ -27,4 +31,15 @@
   ];
 
   home.stateVersion = "25.11";
+
+  # HACK: workaround for sops-nix file missing.
+  # see https://github.com/Mic92/sops-nix/issues/890
+  launchd.agents.sops-nix = pkgs.lib.mkIf pkgs.stdenv.isDarwin {
+    enable = true;
+    config = {
+      EnvironmentVariables = {
+        PATH = pkgs.lib.mkForce "/usr/bin:/bin:/usr/sbin:/sbin";
+      };
+    };
+  };
 }
