e9cc7548828fa92d2edeafe7b599efdad0db5a3c
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-06-17 20:34:40 +0200

    fix(immich): use https pocket id issuer

--- gitops/espresso/tofu/pocket-id/outputs.tf
+++ gitops/espresso/tofu/pocket-id/outputs.tf
@@ -28,7 +28,7 @@ output "immich_config_json" {
       clientId                = pocketid_client.immich.id
       clientSecret            = pocketid_client.immich.client_secret
       enabled                 = true
-      issuerUrl               = "http://pocket-id-internal.pocket-id.svc.cluster.local:1411"
+      issuerUrl               = "https://id.asgard.michaelbrusegard.com"
       mobileOverrideEnabled   = true
       mobileRedirectUri       = "https://photos.asgard.michaelbrusegard.com/api/oauth/mobile-redirect"
       scope                   = "openid email profile"
