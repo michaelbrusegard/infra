66932195186c1e53276c277050d09ed0b40ba04e
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-08-08 20:34:01 -0700

    chore(zen-browser): remove ChatGPT pinned tab

--- modules/home/zen-browser/pins.nix
+++ modules/home/zen-browser/pins.nix
@@ -1,38 +1,31 @@
 _: {
   programs.zen-browser.profiles."default".pins = {
-    "ChatGPT" = {
-      id = "fcfb236d-64d8-4d97-8871-3b720e03ce70";
-      url = "https://chatgpt.com/";
-      workspace = "540f99e5-b487-46f8-9b1a-a91796f0908e";
-      position = 0;
-      isEssential = true;
-    };
     "GitHub" = {
       id = "02cdb6d4-bf59-446f-b8ef-3b7a083cb1fb";
       url = "https://github.com/";
       workspace = "540f99e5-b487-46f8-9b1a-a91796f0908e";
-      position = 1;
+      position = 0;
       isEssential = true;
     };
     "YouTube" = {
       id = "67081b25-e880-468c-a14d-7fc037315051";
       url = "https://www.youtube.com/";
       workspace = "540f99e5-b487-46f8-9b1a-a91796f0908e";
-      position = 2;
+      position = 1;
       isEssential = true;
     };
     "Messenger" = {
       id = "fdb92a64-ed74-48bd-8904-407a6c5129f2";
       url = "https://www.messenger.com/";
       workspace = "540f99e5-b487-46f8-9b1a-a91796f0908e";
-      position = 3;
+      position = 2;
       isEssential = true;
     };
     "WhatsApp" = {
       id = "88437416-b86b-45af-a724-a4338e897ac8";
       url = "https://web.whatsapp.com/";
       workspace = "540f99e5-b487-46f8-9b1a-a91796f0908e";
-      position = 4;
+      position = 3;
       isEssential = true;
     };
   };
