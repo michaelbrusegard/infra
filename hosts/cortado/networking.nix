2ce28bf94bd634fa4b6231c77458fa98257c430f
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-08-30 00:23:30 -0700

    fix(cortado): move the vacuum reservation to .54

--- hosts/cortado/networking.nix
+++ hosts/cortado/networking.nix
@@ -200,7 +200,7 @@ in {
         dhcp-host = [
           "74:fa:29:26:eb:42,10.0.15.2,unifi-ap"
           "30:23:03:06:6c:3c,10.0.15.10,forte"
-          "00:ae:f7:6e:83:75,10.0.17.2,dreame-vacuum"
+          "00:ae:f7:6e:83:75,10.0.17.54,dreame-vacuum"
         ];
         dhcp-option = [
           "tag:trusted,option:router,10.0.15.1"
